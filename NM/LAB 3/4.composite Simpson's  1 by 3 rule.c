bd2f8602dbd3488a4ccfb5280c701f17c4557a11
Nissan Dhakal <nissandhakal11@gmail.com>
2024-04-03 18:15:40 +0545

    Unit 3 lap update

---
+++ NM/LAB 3/4.composite Simpson's  1 by 3 rule.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <math.h>
+float f(float x)
+{
+    return (x * x) * (cos(x));
+}
+int main()
+{
+    int n, m, i;
+    float a, b, h, sum = 0.0, ics, x;
+    printf("\t\t******Composite Simpson's 1/3 ******\n");
+    printf("\n Give lower & upper limit of integration a,b: ");
+    scanf("%f%f", &a, &b);
+    do
+    {
+        printf("\n Give number of segments n (Even number):");
+        scanf("%d", &n);
+    } while (n % 2 != 0);
+    h = (b - a) / n;
+    m = n / 2;
+    for (i = 1; i <= m; i++)
+    {
+        x = a + (2 * i - 1) * h;
+        sum = sum + 4 * f(x);
+        if (i != m)
+            sum = sum + 2 * f(x + h);
+    }
+    sum = sum + f(a) + f(b);
+    ics = sum * h / 3.0;
+    printf("\n Integration between %f and %f when h=%f is %f.", a, b, h, ics);
+
+    return 0;
+}
---
+++ NM/LAB 3/5.composite Simpson's 3 by 8 rule.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+
+float f(float x)
+{
+    return exp(-x * x);
+}
+int main()
+{
+    int n, m, i;
+    float a, b, h, sum = 0.0, ics, x;
+   printf("\t\t******Composite Simpson's 3/8******\n");
+    printf("\n Give lower & upper limits  a,b :");
+    scanf("%f %f", &a, &b);
+    do
+    {
+        printf("\n Give number of segments n(divisible by 3):");
+        scanf("%d", &n);
+    } while (n % 3 != 0);
+    
+    h = (b - a) / n;
+    m = n / 3;
+    for (i = 1; i <= m; i++)
+    {
+        x = a + (3 * i - 2) * h;
+        sum = sum + 3 * (f(x) + f(x + h));
+        if (i != m)
+            sum = sum + 2 * f(x + 2 * h);
+    }
+    sum = sum + f(a) + f(b);
+    ics = sum * 3 * h / 8.0;
+    printf("\nIntegration between %f and %f when h = %f is %f.", a, b, h, ics);
+    return 0;
+}
