5214367d568a570d54526d1175440f0d12910e51
Nissan Dhakal <nissandhakal11@gmail.com>
2024-04-03 18:23:55 +0545

    unit 3 lab update

--- NM/LAB 3/2.central difference formula.c
+++ NM/LAB 3/2.central difference formula.c
@@ -19,6 +19,5 @@ int main()
     fd1 = (f(a + h) - f(a - h)) / (2 * h);
     fd2 = (f(a + h) - 2 * f(a) + f(a - h)) / (h * h);
     printf("\nTHe first and second derivative at x=%f are %f and %f respectively", a, fd1, fd2);
-
     return 0;
 }
