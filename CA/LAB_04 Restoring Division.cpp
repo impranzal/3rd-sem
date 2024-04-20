f1e4ce0fef91516dbc49d37bf37b60c11da0f3b6
Nissan Dhakal <nissandhakal11@gmail.com>
2024-04-20 22:31:30 +0545

    lab add

---
+++ CA/LAB_04 Restoring Division.cpp
@@ -0,0 +1,167 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int a = 0, b = 0, c = 0, com[5] = {1, 0, 0, 0, 0};
+int anum[5] = {0}, bnum[5] = {0}, anumcp[5] = {0};
+int acomp[5] = {0}, bcomp[5] = {0}, rem[5] = {0}, quo[5] = {0}, res[5] = {0};
+
+void binary()
+{
+    a = abs(a);
+    b = abs(b);
+    int r, r2, i;
+    for (i = 0; i < 5; i++)
+    {
+        r = a % 2;
+        a /= 2;
+        r2 = b % 2;
+        b /= 2;
+        anum[i] = r;
+        anumcp[i] = r;
+        bnum[i] = r2;
+        if (r2 == 0)
+            bcomp[i] = 1;
+        if (r == 0)
+            acomp[i] = 1;
+    }
+    c = 0;
+    for (i = 0; i < 5; i++)
+    {
+        res[i] = com[i] + bcomp[i] + c;
+        if (res[i] >= 2)
+            c = 1;
+        else
+            c = 0;
+        res[i] = res[i] % 2;
+    }
+    for (i = 4; i >= 0; i--)
+        bcomp[i] = res[i];
+}
+
+void add(int num[])
+{
+    int i, c = 0;
+    for (i = 0; i < 5; i++)
+    {
+        res[i] = rem[i] + num[i] + c;
+        if (res[i] >= 2)
+            c = 1;
+        else
+            c = 0;
+        res[i] = res[i] % 2;
+    }
+    for (i = 4; i >= 0; i--)
+    {
+        rem[i] = res[i];
+        cout << rem[i];
+    }
+    cout << " : ";
+    for (i = 4; i >= 0; i--)
+        cout << anumcp[i];
+}
+
+void shl()
+{
+    int i;
+    for (i = 0; i < 4; i++)
+        rem[i] = rem[i + 1];
+    rem[4] = anumcp[0];
+    for (i = 4; i > 0; i--)
+        anumcp[i] = anumcp[i - 1];
+    anumcp[0] = 0;
+    cout << "\nSHIFT LEFT: ";
+    for (i = 4; i >= 0; i--)
+        cout << rem[i];
+    cout << " : ";
+    for (i = 4; i >= 0; i--)
+        cout << anumcp[i];
+}
+
+int main()
+{
+    int i;
+    cout << "\t\t******RESTORING DIVISION******" << endl
+         << endl;
+    cout << "Enter two numbers to divide" << endl;
+    cout << "Both numbers should be less than 16" << endl
+         << endl;
+
+    do
+    {
+        cout << "Enter the dividend: ";
+        cin >> a;
+        cout << "Enter the divisor: ";
+        cin >> b;
+    } while (a >= 16 || b >= 16);
+
+    cout << endl
+         << "Expected Quotient: " << a / b << endl;
+    cout << "Expected Remainder: " << a % b << endl;
+
+    if (pow(a, b) < 0)
+        c = 1;
+    binary();
+
+    cout << endl
+         << endl
+         << "Unsigned Binary Equivalents are: " << endl;
+    cout << "A: ";
+    for (i = 4; i >= 0; i--)
+        cout << anum[i];
+
+    cout << endl
+         << "B: ";
+    for (i = 4; i >= 0; i--)
+        cout << bnum[i];
+
+    cout << endl
+         << "B'+1: ";
+    for (i = 4; i >= 0; i--)
+        cout << bcomp[i];
+
+    cout << endl
+         << endl
+         << "-->";
+    shl();
+
+    for (i = 0; i < 5; i++)
+    {
+        cout << endl
+             << "-->";
+        cout << endl
+             << "SUB B: ";
+        add(bcomp);
+
+        if (rem[4] == 1)
+        {
+            cout << endl
+                 << "--> RESTORE" << endl
+                 << "ADD B: ";
+            anumcp[0] = 0;
+            add(bnum);
+        }
+        else
+        {
+            anumcp[0] = 1;
+        }
+        if (i < 4)
+        {
+            shl();
+        }
+    }
+    cout << endl
+         << "----------------------------";
+    cout << endl
+         << "Sign of result: " << c;
+    cout << endl
+         << "Remainder: ";
+    for (i = 4; i >= 0; i--)
+        cout << rem[i];
+    cout << endl
+         << "Quotient: ";
+    for (i = 4; i >= 0; i--)
+        cout << anumcp[i];
+
+    return 0;
+}
---
+++ CA/LAB_05 Non-restoring Division.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+using namespace std;
+
+int a[5] = {0, 0, 0, 0, 0};
+int q[4] = {0, 0, 0, 0};
+int b[5] = {0, 0, 0, 0, 0};
+int b2c[5] = {0, 0, 0, 0, 0};
+
+void comp() {
+    int i = 4;
+    while (i >= 0 && b[i] != 1) {
+        b2c[i] = b[i];
+        i--;
+    }
+    while (i >= 0) {
+        b2c[i] = (b[i] + 1) % 2;
+        i--;
+    }
+    cout << "\n\tB's complement:";
+    for (i = 0; i < 5; i++) {
+        cout << b2c[i];
+    }
+    cout << endl;
+}
+
+void nonresdiv() {
+    for (int i = 0; i < 4; i++) {
+        if (a[0] == 0) {
+            int temp = a[4];
+            for (int j = 4; j > 0; j--) {
+                a[j] = a[j - 1];
+            }
+            a[0] = q[3];
+            for (int j = 3; j > 0; j--) {
+                q[j] = q[j - 1];
+            }
+            q[0] = 0;
+            for (int j = 0; j < 5; j++) {
+                int sum = a[j] + b2c[j] + 0;
+                a[j] = sum % 2;
+            }
+        } else {
+            int temp = a[4];
+            for (int j = 4; j > 0; j--) {
+                a[j] = a[j - 1];
+            }
+            a[0] = q[3];
+            for (int j = 3; j > 0; j--) {
+                q[j] = q[j - 1];
+            }
+            q[0] = 1;
+            for (int j = 0; j < 5; j++) {
+                int sum = a[j] + b[j] + 0;
+                a[j] = sum % 2;
+            }
+        }
+    }
+}
+
+int main() {
+    cout<<"\t\t******NON-RESTORING DIVISION******\n\n";
+    cout << "Enter dividend in binary form (4 bits): ";
+    for (int i = 0; i < 4; i++) {
+        cin >> q[i];
+    }
+
+    cout << "Enter divisor in binary form (5 bits): ";
+    for (int i = 0; i < 5; i++) {
+        cin >> b[i];
+    }
+
+    comp();
+
+    cout << "\n\t[A]\t[M]\n";
+    for (int i = 0; i < 4; i++) {
+        nonresdiv();
+        cout << "\t";
+        for (int j = 0; j < 5; j++) {
+            cout << a[j];
+        }
+        cout << "\t";
+        for (int k = 0; k < 4; k++) {
+            cout << q[k];
+        }
+        cout << endl;
+    }
+
+    if (a[0] == 1) {
+        for (int i = 0; i < 5; i++) {
+            int sum = a[i] + b2c[i] + 0;
+            a[i] = sum % 2;
+        }
+    }
+
+    cout << "\t";
+    for (int j = 0; j < 5; j++) {
+        cout << a[j];
+    }
+
+    cout << "\t";
+    for (int k = 0; k < 4; k++) {
+        cout << q[k];
+    }
+    cout << endl;
+
+    return 0;
+}
