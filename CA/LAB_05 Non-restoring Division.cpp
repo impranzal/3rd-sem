a6dbe9466bc42151223a11925f83e2a3cba469ec
Nissan Dhakal <nissandhakal11@gmail.com>
2024-04-22 23:43:58 +0545

    REMOVE unused variables

--- CA/LAB_05 Non-restoring Division.cpp
+++ CA/LAB_05 Non-restoring Division.cpp
@@ -6,50 +6,65 @@ int q[4] = {0, 0, 0, 0};
 int b[5] = {0, 0, 0, 0, 0};
 int b2c[5] = {0, 0, 0, 0, 0};
 
-void comp() {
+void comp()
+{
     int i = 4;
-    while (i >= 0 && b[i] != 1) {
+    while (i >= 0 && b[i] != 1)
+    {
         b2c[i] = b[i];
         i--;
     }
-    while (i >= 0) {
+    while (i >= 0)
+    {
         b2c[i] = (b[i] + 1) % 2;
         i--;
     }
     cout << "\n\tB's complement:";
-    for (i = 0; i < 5; i++) {
+    for (i = 0; i < 5; i++)
+    {
         cout << b2c[i];
     }
     cout << endl;
 }
 
-void nonresdiv() {
-    for (int i = 0; i < 4; i++) {
-        if (a[0] == 0) {
+void nonresdiv()
+{
+    for (int i = 0; i < 4; i++)
+    {
+        if (a[0] == 0)
+        {
             int temp = a[4];
-            for (int j = 4; j > 0; j--) {
+            for (int j = 4; j > 0; j--)
+            {
                 a[j] = a[j - 1];
             }
             a[0] = q[3];
-            for (int j = 3; j > 0; j--) {
+            for (int j = 3; j > 0; j--)
+            {
                 q[j] = q[j - 1];
             }
             q[0] = 0;
-            for (int j = 0; j < 5; j++) {
+            for (int j = 0; j < 5; j++)
+            {
                 int sum = a[j] + b2c[j] + 0;
                 a[j] = sum % 2;
             }
-        } else {
+        }
+        else
+        {
             int temp = a[4];
-            for (int j = 4; j > 0; j--) {
+            for (int j = 4; j > 0; j--)
+            {
                 a[j] = a[j - 1];
             }
             a[0] = q[3];
-            for (int j = 3; j > 0; j--) {
+            for (int j = 3; j > 0; j--)
+            {
                 q[j] = q[j - 1];
             }
             q[0] = 1;
-            for (int j = 0; j < 5; j++) {
+            for (int j = 0; j < 5; j++)
+            {
                 int sum = a[j] + b[j] + 0;
                 a[j] = sum % 2;
             }
@@ -57,48 +72,58 @@ void nonresdiv() {
     }
 }
 
-int main() {
-    cout<<"\t\t******NON-RESTORING DIVISION******\n\n";
+int main()
+{
+    cout << "\t\t******NON-RESTORING DIVISION******\n\n";
     cout << "Enter dividend in binary form (4 bits):\n ";
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < 4; i++)
+    {
         cin >> q[i];
     }
 
     cout << "Enter divisor in binary form (5 bits):\n ";
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 5; i++)
+    {
         cin >> b[i];
     }
 
     comp();
 
     cout << "\n\t[A]\t[M]\n";
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < 4; i++)
+    {
         nonresdiv();
         cout << "\t";
-        for (int j = 0; j < 5; j++) {
+        for (int j = 0; j < 5; j++)
+        {
             cout << a[j];
         }
         cout << "\t";
-        for (int k = 0; k < 4; k++) {
+        for (int k = 0; k < 4; k++)
+        {
             cout << q[k];
         }
         cout << endl;
     }
 
-    if (a[0] == 1) {
-        for (int i = 0; i < 5; i++) {
+    if (a[0] == 1)
+    {
+        for (int i = 0; i < 5; i++)
+        {
             int sum = a[i] + b2c[i] + 0;
             a[i] = sum % 2;
         }
     }
 
     cout << "\t";
-    for (int j = 0; j < 5; j++) {
+    for (int j = 0; j < 5; j++)
+    {
         cout << a[j];
     }
 
     cout << "\t";
-    for (int k = 0; k < 4; k++) {
+    for (int k = 0; k < 4; k++)
+    {
         cout << q[k];
     }
     cout << endl;
